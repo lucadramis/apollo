3c500f178965bd356709e97f85634dc9c7af4b9b
luca <lucadid3503@aol.com>
2019-02-20 20:08:20 +0100

    19/2

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import logo from './logo.svg';
 import { ApolloClient } from 'apollo-client'
 import { ApolloProvider } from "react-apollo";
 import Utente from './components/spotify';
-
 import { InMemoryCache } from 'apollo-cache-inmemory'
 import { createHttpLink } from 'apollo-link-http'
 import { onError } from 'apollo-link-error'
--- src/components/spotify.jsx
+++ src/components/spotify.jsx
@@ -1,9 +1,41 @@
 import React, { Component } from 'react';
 import {Query} from "react-apollo";
 import gql from "graphql-tag";
+import { Collapse, Button, Table, } from 'reactstrap';
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faIgloo } from '@fortawesome/free-solid-svg-icons'
+
+library.add(faIgloo)
+
+const humanizeDuration = require('humanize-duration')
+humanizeDuration(12000) // '12 seconds'
+
+const shortEnglishHumanizer = humanizeDuration.humanizer({
+    language: 'shortEn',
+    languages: {
+      shortEn: {
+        m: () => '',
+        s: () => '',
+      }
+    },
+    round: true,
+    conjunction:':'
+  })
+  
+  shortEnglishHumanizer(15600000)
 
 export default class Utente extends 
 Component {
+    constructor(props) {
+        super(props);
+        this.toggle = this.toggle.bind(this);
+        this.state = { collapse: false };
+      }
+    
+      toggle() {
+        this.setState({ collapse: !this.state.collapse });
+      }
     render (){
         return(
         <Query query={gql`
@@ -11,10 +43,10 @@ Component {
   
             me  {
                 display_name    
-               country
+               
               href
               email
-              uri
+              
              albums {
                added_at
              }
@@ -24,17 +56,29 @@ Component {
                 width
               }
               top_tracks {
-                id
-                album{
-                    name
-                    artists 
-                {
-                 name
-                    }
-                }
-              }top_artists {
                 name
+                track_number
+                duration_ms
+                artists{
+                  name
+                }
+                album{
+                  name
+                }
+                preview_url
               }
+              top_artists {
+    
+                name
+                genres
+                albums{
+                  name
+                  tracks{
+                    track_number
+                    duration_ms
+                    preview_url
+                  }
+                }
             
                    
               }
@@ -47,15 +91,81 @@ Component {
                     {!loading && 
                     <div>
                         {/*JSON.stringify(data, null, 2)*/}
-                        <img className="imguser" src={data.me.images[0].url} width="100" height="100"></img>
+                        {/*<img className="imguser" src={data.me.images[0].url} width="100" height="100"></img>*/}
                         <div>nome utente: {data.me.display_name}</div>
                         <div>link: {data.me.href}</div>
                         <div>email :{data.me.email}</div>
-                        <div> {data.me.top_tracks.map((item)=>
-                            <div>{item.album.artists[0].name}</div>)}</div>
+
+                        <Button color="primary" onClick={this.toggle} style={{ marginBottom: '1rem' }}>Top Tracks</Button>
+                    <Collapse isOpen={this.state.collapse}>
+                    <div className="top_tracks">
+
+                            <Table dark>
+                              <thead>
+                                <tr>
+                                  <th>#</th>
+                                  <th>Duration</th>
+                                  <th>Name</th>
+                                  <th>Artist</th>
+                                  <th>Album</th>
+                                  <th>Play</th>
+                                </tr>
+                              </thead>
+                              <tbody>
+
+                                {data.me.top_tracks.map((item, idx) =>
+                                  <tr>
+                                    <td scope="row"><div>{idx + 1}</div></td>
+
+                                    <td> <div>{shortEnglishHumanizer(item.duration_ms)}</div></td>
+                                    <td><div>{item.name}</div></td>
+                                    <td><div>{item.artists[0].name}</div></td>
+                                    <td><div>{item.album.name}</div></td>
+                                    <td><div><a href={item.preview_url}><FontAwesomeIcon icon="play" /><i class="fas fa-play"></i></a></div></td>
+                                  </tr>
+                                )}
+                              </tbody>
+                            </Table>
+                          </div>
+                    </Collapse>
+                    <Collapse isOpen={this.state.collapse}>
+                    <div className="top_artists">
+
+                            <Table dark>
+                              <thead>
+                                <tr>
+                                  <th>#</th>
+                                  <th>Duration</th>
+                                  <th>Name</th>
+                                  <th>Artist</th>
+                                  <th>Album</th>
+                                  <th>Play</th>
+                                </tr>
+                              </thead>
+                              <tbody>
+
+                                {data.me.top_artists.map((item, idx) =>
+                                  <tr>
+                                    <td scope="row"><div>{idx + 1}</div></td>
+
+                                    <td> <div>{shortEnglishHumanizer(item.duration_ms)}</div></td>
+                                    <td><div>{item.name}</div></td>
+                                    <td><div>{item.artists[0].name}</div></td>
+                                    <td><div>{item.album.name}</div></td>
+                                    <td><div><a href={item.preview_url}><FontAwesomeIcon icon="play" /><i class="fas fa-play"></i></a></div></td>
+                                  </tr>
+                                )}
+                              </tbody>
+                            </Table>
+                          </div>
+                    </Collapse>
+                    
+                        
                         {/*<div>top tracs : {data.me.top_tracks[0].album.name}</div>*/}
+                        
                         <div> {data.me.top_artists.map((item)=>
-                            <div>{item.name}</div>)}</div>
+                            <div>{item.name}</div>)}
+                        </div>
                         {/*<div>top artist : {data.me.top_artists[0].name}</div>*/}
                         
                         
